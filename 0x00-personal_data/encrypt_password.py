659221af7e70c7900b4d6e45c6a488b12ae9c1d9
Carey99 <edwinakidah1@gmail.com>
2024-08-02 05:15:16 +0300

    Add password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Encrypting passwords"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
