48952ad35419289e582f054916b086605419b0a4
Carey99 <edwinakidah1@gmail.com>
2024-08-15 23:42:16 +0300

    Generate a reset oken

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -62,3 +62,11 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         self._db.update_user(user_id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generates a reset token for a given email.
+        """
+        user = self._db.find_user_by(email=email)
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
