ef4576f999e72c106c69fb46daf5d24d0c12e5b6
Carey99 <edwinakidah1@gmail.com>
2024-08-15 21:19:53 +0300

    Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,8 +5,25 @@
     hasheddd with bcrypt
 """
 import bcrypt
+from db import DB, NoResultFound
+from user import User
 
 
-def _hash_password(password: str) -> bytes:
-    """returns a salted hash of the input"""
-    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def _hash_password(self, password: str) -> bytes:
+        """returns a salted hash of the input"""
+        return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+    def register_user(self, email: str, password: str) -> User:
+        """register a new user"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            return self._db.add_user(email, self._hash_password(password))
