fa400d19b09787cc8e579898ff46d956c6a46bf0
Carey99 <edwinakidah1@gmail.com>
2024-08-15 23:39:53 +0300

    Loog  out buuttton

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -48,17 +48,17 @@ class Auth:
         return session_id
 
     def get_user_from_session_id(self, session_id: str) -> User:
+        """Retrieves a user based on a given session ID.
+        """
+        user = None
+        if session_id is None:
+            return None
         try:
-            return self._db.find_user_by(session_id=session_id)
+            user = self._db.find_user_by(session_id=session_id)
         except NoResultFound:
             return None
+        return user
 
     def destroy_session(self, user_id: int) -> None:
         self._db.update_user(user_id, session_id=None)
         return None
-
-    def get_user_from_session_id(self, session_id: str) -> User:
-        try:
-            return self._db.find_user_by(session_id=session_id)
-        except NoResultFound:
-            return None
