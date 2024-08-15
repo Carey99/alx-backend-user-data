2eb622b8de941cd03cd68e021d4d42f2e96de300
Carey99 <edwinakidah1@gmail.com>
2024-08-15 20:04:17 +0300

    Find  user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 import bcrypt
 
 from user import Base, User
@@ -37,3 +39,12 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by a given attribute"""
+        try:
+            return self._session.query(User).filter_by(**kwargs).first()
+        except NoResultFound:
+            return None
+        except InvalidRequestError:
+            return None
